257b9bd763be7033be4edd78c03aece137921a78
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-03-06 14:48:22 -0800

    api changes associated with new runner interface

--- tegrity/apt.py
+++ tegrity/apt.py
@@ -18,17 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import hashlib
 import logging
 import os
+import pathlib
 import subprocess
 
-import tegrity
+import tegrity.utils
 
 from typing import (
     Iterable,
+    List,
     MutableSet,
     Optional,
     Text,
+    Union,
 )
 
 # temporary folder to copy .deb files to:
@@ -40,7 +44,10 @@ REQUIREMENTS = {
     "bc",
     "build-essential",
     "libncurses5-dev",  # needed by tegrity.kernel so menuconfig works
+    "python",  # required by some sdkmanager scripts but not listed in .deb deps
+    "python3-pip",  # needed to install tegrity with install.py
     "qemu-user-static",  # needed by tegrity.qemu
+    'proot',  # needed to avoid elevated privileges requirements
 }
 
 USAGE = """usage examples:
@@ -73,94 +80,153 @@ __all__ = [
 ]
 
 
-def list_installed(runner=None) -> MutableSet[Text]:
+def list_(runner=tegrity.utils,
+          installed=False,
+          sorted_=False,
+          ) -> Union[MutableSet[Text], List[Text]]:
     """
-    :returns: a MutableSet (eg. set()) of installed packages on the system
-    :raises: subprocess.CalledProcessError if command fails
+    runs: apt list
 
-    >>> "apt" in list_installed()
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+    :param installed: same as list_installed(), returns installed packages
+    instead of available packages
+    :param sorted_: returns a list in alphabetical order, rather than a Set
+
+    :returns: a MutableSet (eg. set()) of available packages
+    :raises: subprocess.CalledProcessError if command fails
+    >>> "apt" in list_()
     True
     """
-    logger.debug("Checking for apt requirements...")
-    runner = runner.run_cmd if runner else tegrity.utils.run
-    cp = tegrity.utils.run(
-        ("apt", "list", "--installed"),
+    logger.debug(f"checking{' installed ' if installed else ' available '}"
+                 f"apt packages...")
+    command = ["apt", "list"]
+    if installed:
+        command.append('--installed')
+    cp = runner.run(
+        command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,  # to suppress the apt usage in script warning
         universal_newlines=True,
     )  # type: subprocess.CompletedProcess
     cp.check_returncode()
+    if sorted_:
+        return [line.split('/')[0] for line in cp.stdout.split('\n')]
     return {line.split('/')[0] for line in cp.stdout.split('\n')}
 
 
-def update(runner=None) -> subprocess.CompletedProcess:
+def list_installed(runner=tegrity.utils,
+                   sorted_=False,
+                   ) -> Union[MutableSet[Text], List[Text]]:
     """
-    :returns: subprocess.CompletedProcess instance for 'sudo apt-get update'
+    runs: apt list --installed
+
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+    :param sorted_: returns a list in alphabetical order, rather than a Set
+
+    :returns: a MutableSet (eg. set()) of available packages
+    :raises: subprocess.CalledProcessError if command fails
+    >>> "apt" in list_installed()
+    True
+    """
+    return list_(runner=runner, installed=True, sorted_=sorted_)
+
+
+def update(runner=tegrity.utils) -> subprocess.CompletedProcess:
+    """
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+
+    :returns: a subprocess.CompletedProcess for the command
     :raises: subprocess.CalledProcessError if command fails
     """
-    logger.info("Updating package information.")
-    command = ('apt-get', 'update')
-    if runner:
-        return runner.run_cmd(command)
-    else:
-        return tegrity.utils.run(command)
+    return runner.run(('apt-get', 'update'))
 
 
-def upgrade(runner: tegrity.qemu.QemuRunner = None,
+def upgrade(runner=tegrity.utils,
             dist_upgrade: bool = True,
-            autoremove: bool = False,):
-    logger.info(f"updating system at {runner.rootfs if runner else '/'}")
+            autoremove_: bool = False,) -> subprocess.CompletedProcess:
+    """
+    runs: apt-get upgrade
+
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+    :param dist_upgrade: dist-upgrade instead of upgrade. see apt-get manual
+    :param autoremove_: adds --autoremove, see apt-get manual
+
+    :returns: a subprocess.CompletedProcess for the command
+    :raises: subprocess.CalledProcessError if command fails
+    """
     command = ['apt-get', 'dist-upgrade' if dist_upgrade else 'upgrade', '-y']
-    if autoremove:
+    if autoremove_:
         command.append('--autoremove')
-    if runner:
-        return runner.run_cmd(command)
-    else:
-        return tegrity.utils.run(command)
+    return runner.run(command)
 
 
-def install(deps: Iterable[Text],
-            runner: tegrity.qemu.QemuRunner = None,
+def install(packages: Iterable[Text],
+            runner=tegrity.utils,
+            no_install_recommends=False,
             ) -> subprocess.CompletedProcess:
     """
-    :returns: subprocess.CompletedProcess for subprocess.run() of:
-              sudo apt-get install -y *sorted(set(deps))
+    runs: apt-get install -y *sorted(set(packages))
 
-    :arg deps: Iterable of apt dependencies
+    :arg packages: Iterable of apt package names
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+    :param no_install_recommends: does not install recommended packages
+    see apt manual for --no-install-recommends
 
-    :param runner: tegrity.qemu.QemuRunner to run the command with
-
+    :returns: a subprocess.CompletedProcess for the command
     :raises: subprocess.CalledProcessError if command fails
     """
-    command = ('apt-get', 'install', '-y', *sorted(set(deps)))
-    if runner:
-        return runner.run_cmd(command)
-    else:
-        return tegrity.utils.run(command)
+    command = ['apt-get', 'install', '-y']
+    if no_install_recommends:
+        command.append('--no-install-recommends')
+    command.extend(sorted(set(packages)))
+    return runner.run(command)
 
 
-def remove(packages: Iterable[Text], runner: tegrity.qemu.QemuRunner = None):
-    command = ('apt-get', 'remove', *sorted(set(packages)))
-    if runner:
-        return runner.run_cmd(command)
-    else:
-        return tegrity.utils.run(command)
+def remove(packages: Iterable[Text],
+           runner=tegrity.utils,
+           ) -> subprocess.CompletedProcess:
+    """
+    runs: apt-get remove *sorted(set(packages))
+
+    :arg packages: Iterable of apt package names
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+
+    :returns: a subprocess.CompletedProcess for the command
+    :raises: subprocess.CalledProcessError if command fails
+    """
+    command = ('apt-get', 'remove', '-y', *sorted(set(packages)))
+    return runner.run(command)
 
 
-def purge(packages: Iterable[Text], runner: tegrity.qemu.QemuRunner = None):
-    command = ('apt-get', 'purge', *sorted(set(packages)))
-    if runner:
-        return runner.run_cmd(command)
-    else:
-        return tegrity.utils.run(command)
+def purge(packages: Iterable[Text],
+          runner=tegrity.utils,
+          ) -> subprocess.CompletedProcess:
+    """
+    runs: apt-get purge *sorted(set(packages))
+
+    :arg packages: Iterable of apt package names
+    :param runner: object/module with .run() method such as
+    tegrity.utils, tegrity.qemu.QemuRunner or the subprocess module
+
+    :returns: a subprocess.CompletedProcess for the command
+    :raises: subprocess.CalledProcessError if command fails
+    """
+    command = ('apt-get', 'purge', '-y', *sorted(set(packages)))
+    return runner.run(command)
 
 
-def clean(runner: tegrity.qemu.QemuRunner = None):
-    command = ('apt-get', 'clean')
-    if runner:
-        return runner.run_cmd(command)
-    else:
-        return tegrity.utils.run(command)
+def clean(runner=tegrity.utils) -> subprocess.CompletedProcess:
+    return runner.run(('apt-get', 'clean'))
+
+
+def autoremove(runner=tegrity.utils) -> subprocess.CompletedProcess:
+    return runner.run(('apt-get', 'autoremove'))
 
 
 def autoremove(runner: tegrity.qemu.QemuRunner = None):
@@ -216,7 +282,7 @@ def qemu_dpkg_install(runner: tegrity.qemu.QemuRunner, filenames: Iterable):
             dest_filenames.append(dest)
             abspath_filenames.append(
                 os.path.join('/', *DPKG_FOLDER, os.path.basename(filename)))
-            runner.run_cmd(('dpkg', '-i', *abspath_filenames)).check_returncode()
+            runner.run(('dpkg', '-i', *abspath_filenames)).check_returncode()
     except Exception as err:
         logger.error(
             f"dpkg install on rootfs had error. "
@@ -238,41 +304,59 @@ def qemu_dpkg_install(runner: tegrity.qemu.QemuRunner, filenames: Iterable):
 
 
 def main(rootfs,
+         nvidia_ota: Optional[bool] = None,
+         proot: Optional[bool] = None,
          apt_upgrade: Optional[bool] = None,
+         apt_autoremove: Optional[bool] = True,
          apt_install: Optional[Iterable] = None,
+         no_install_recommends: Optional[bool] = False,
          apt_remove: Optional[Iterable] = None,
          apt_purge: Optional[Iterable] = None,
+         apt_clean: Optional[bool] = True,
          debs: Optional[Iterable] = None,):
     """
     Does apt operations on a rootfs.
 
     :arg rootfs: the rootfs to operate on
+    :param nvidia_ota: patch apt sources on an l4t rootfs with board id
+    :param proot: uses proot instead of chroot (no need for root permissions)
     :param apt_upgrade: whether to upgrade the system
+    :param apt_autoremove: does an apt autoremove at the end, before apt clean
     :param apt_install: optional iterable of packages to install
+    :param no_install_recommends: see install()
     :param apt_remove: optional iterable of packages to remove
     :param apt_purge: optional iterable of packages and configuration to remove
+    :param apt_clean: do an apt clean at the end (default True)
     :param debs: optional iterable of debian packages to install
-    :return:
     """
+    if nvidia_ota:
+        enable_nvidia_ota(rootfs)
     if not (apt_upgrade or apt_install or apt_remove or apt_purge or debs):
-        raise ValueError("Nothing to do...")
-    with tegrity.qemu.QemuRunner(rootfs) as runner:
+        return
+    runner_cls = tegrity.qemu.ProotRunner if proot else tegrity.qemu.QemuRunner
+    with runner_cls(rootfs) as runner:
         try:
             if apt_upgrade or apt_install:
                 update(runner=runner).check_returncode()
             if apt_upgrade:
                 upgrade(runner=runner).check_returncode()
             if apt_install:
-                install(apt_install, runner=runner).check_returncode()
+                install(
+                    apt_install,
+                    runner=runner,
+                    no_install_recommends=no_install_recommends,
+                ).check_returncode()
             if apt_remove:
                 remove(apt_remove, runner=runner)
             if apt_purge:
                 purge(apt_purge, runner=runner)
             if debs:
                 qemu_dpkg_install(runner, debs)
-            autoremove(runner)
+            if apt_autoremove:
+                autoremove(runner)
         finally:
-            clean(runner)
+            if apt_clean:
+                clean(runner)
 
 
 def cli_main():
@@ -286,6 +370,14 @@ def cli_main():
     ap.add_argument(
         'rootfs', help="the rootfs path to operate on")
     ap.add_argument(
+        '--nvidia-ota', help="patches nvidia apt sources list with correct SOC"
+        " id so OTA updates work before first boot.",
+        action='store_true')
+    ap.add_argument(
+        '--proot', help="use proot instead of chroot (no root permisions needed)",
+        action='store_true',
+        default=tegrity.settings.IN_DOCKER,)
+    ap.add_argument(
         '--upgrade', help="does a full upgrade with --autoremove",
         dest='apt_upgrade',
         action='store_true')
@@ -293,6 +385,9 @@ def cli_main():
         '--install', help="installs apt packages from online repos",
         dest='apt_install',
         nargs='+')
+    ap.add_argument(
+        '--no-install-recommends', help='no recommended packages. see apt manual',
+        action='store_true')
     ap.add_argument(
         '--remove', help="removes apt packages",
         dest='apt_remove',
@@ -302,10 +397,13 @@ def cli_main():
         dest='apt_purge',
         nargs='+')
     ap.add_argument(
+        '--no-clean', help='does no apt clean at the end', dest='apt_clean',
+        action='store_false')
+    ap.add_argument(
         '--debs', help="a list of debian packages to install using dpkg",
         nargs='+',)
 
-    main(**tegrity.cli.cli_common(ap))
+    main(**tegrity.cli.cli_common(ap, ensure_sudo=False))
 
 
 if __name__ == '__main__':
